5a34dcde979789bb11a7f61834a4caeff37e1d99
OleksandrMMelnyk <OleksandrMMelnyk@gmail.com>
2023-11-14 09:27:19 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,33 @@
+package app;
+
+public class Main {
+
+    static String productNumber;
+    static String productName;
+    static double totalSales;
+    static int dayCount;
+    static double daySales;
+    private final static String CURRENCY = "EUR";
+
+    public static void main(String[] args) {
+
+        productNumber = "Product No 1";
+        productName = "smartphone";
+        totalSales = 12153.41;
+        dayCount = 5;
+        daySales = totalSales / dayCount ;
+
+        System.out.printf( "%n%s: %s, %ntotal sales for 5 days is %s %.2f, %nsales by day is %s %.2f.",
+                productNumber, productName, CURRENCY, totalSales, CURRENCY, daySales);
+
+        productNumber = "Product No 2";
+        productName = "laptop";
+        totalSales = 10486.85;
+        dayCount = 7;
+        daySales = totalSales / dayCount ;
+
+        System.out.printf( "%n%s: %s, %ntotal sales for 7 days is %s %.2f, %nsales by day is %s %.2f.",
+                productNumber, productName, CURRENCY, totalSales, CURRENCY, daySales);
+
+    }
+}
